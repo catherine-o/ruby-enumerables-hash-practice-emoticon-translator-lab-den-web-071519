9704e437174b2f8a685266f77271bda82c9f5923
Catherine O'Hara <catherine.ohara@outlook.com>
2019-07-06 01:40:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  if library[get_emoticon].keys.include?(emoticon)
+  if library["get_emoticon"].keys.include?(emoticon)
     library["get_emoticon"][emoticon]
   end
   binding.pry
